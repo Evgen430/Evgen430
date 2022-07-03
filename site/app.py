3875099ed5d4d26170560bd0fcb31b28d17c980d
Evgen <Evgen_430@mail.ru>
2022-07-03 21:02:48 +0500

    попытка добавить базу

--- site/app.py
+++ site/app.py
@@ -1,5 +1,20 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+# import mysql.connector
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+db = SQLAlchemy(app)
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    title = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
 
 @app.route('/')
 @app.route('/home')
@@ -10,7 +25,7 @@ def index():
 def about():
     return render_template("about.html")
 
-@app.route('/blog')
+@app.route('/blog', methods=['POST', 'GET'])
 def blog():
     return render_template("blog.html")
 
