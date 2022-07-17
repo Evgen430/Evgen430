e2e5016df6a9073dac182c2318aa6447f5464b94
Evgen <Evgen_430@mail.ru>
2022-07-17 19:17:13 +0500

    добавление функций редактирования и удаления записей.

--- site/app.py
+++ site/app.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-# import mysql.connector
+import logging
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
 
 class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True, unique=True)
@@ -16,20 +18,79 @@ class Article(db.Model):
     def __repr__(self):
         return '<Article %r>' % self.id
 
+
 @app.route('/')
 @app.route('/home')
 def index():
     return render_template("index.html")
 
+
 @app.route('/about')
 def about():
     return render_template("about.html")
 
+
 @app.route('/blog', methods=['POST', 'GET'])
+# Обработка получения из формы и сохранения записи в базе
 def blog():
-    return render_template("blog.html")
+    if request.method == "POST":
+        title = request.form['title']
+        text = request.form['text']
+
+        article = Article(title=title, text=text)
+
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/blog')
+
+        except Exception:
+            logging.exception('')
+            return ''
+
+    else:
+        articles = Article.query.all()
+        return render_template("blog.html", articles=articles)
+
+
+@app.route('/blog/<int:id>')
+# Обработка отображения записи
+def blog_record(id):
+    article = Article.query.get(id)
+    return render_template("blog_record.html", article=article)
+
+@app.route('/blog/<int:id>/delete')
+# Обработка удаления записи
+def record_delete(id):
+    article = Article.query.get_or_404(id)
+    try:
+        db.session.delete(article)
+        db.session.commit()
+        return redirect('/blog')
+    except:
+        return "При удалении записи произошла ошибка"
+
+@app.route('/blog/<int:id>/update', methods=['POST', 'GET'])
+def record_update(id):
+    article = Article.query.get(id)
+    if request.method == "POST":
+        article.title = request.form['title']
+        article.text = request.form['text']
+
+        try:
+            db.session.commit()
+            return redirect('/blog')
+
+        except Exception:
+            logging.exception('')
+            return ''
+
+    else:
+        return render_template("record_update.html", article=article)
+
 
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
 
 
